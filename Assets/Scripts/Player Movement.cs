fc05dd2224b6152bb08015878ede3e4f475c3cb5
kamphuis123 <kamphuisjulius@gmail.com>
2024-03-13 14:08:20 +0100

    Movement

---
+++ Assets/Scripts/Player Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float LR;
+    public float UD;
+    public float mSpeed;
+    public float rSpeed; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        walkAnim();
+        playerMovement();
+
+
+
+    }
+
+
+    void walkAnim()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("animMultiplier", UD);
+    }
+
+    void playerMovement()
+    {
+
+        LR = Input.GetAxis("Horizontal");
+        UD = Input.GetAxis("Vertical");
+
+        mSpeed = moveSpeed * UD; 
+
+        print(LR);
+        print(UD);
+
+        transform.position += transform.forward * Time.deltaTime * mSpeed;
+        transform.Rotate(new Vector3(0, LR *rSpeed, 0) * Time.deltaTime);
+
+
+
+
+    }
+}
